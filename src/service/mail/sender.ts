672405fd88144746101cdcafc04e3a5db739592f
David Recuenco <david.recuenco@adidas-group.com>
2021-03-09 20:17:51 +0100

    UPDATE mailing with sparkpost

--- src/service/mail/sender.ts
+++ src/service/mail/sender.ts
@@ -1,17 +1,14 @@
 import { createTransport } from 'nodemailer'
-import SMTPTransport = require('nodemailer/lib/smtp-transport')
+import * as sparkPost from 'nodemailer-sparkpost-transport'
 
 export async function sendMail(html: string) {
-  const transport = createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS
-    }
-  } as SMTPTransport.Options)
+  const transport = createTransport(sparkPost({
+    sparkPostApiKey: process.env.SPARKPOST_API_KEY,
+    endpoint: 'https://api.eu.sparkpost.com'
+  }))
 
   await transport.sendMail({
-    from: process.env.GMAIL_USER,
+    from: process.env.SPARKPOST_USER,
     to: process.env.MAIL_TO,
     subject: 'Clases disponibles en Autius',
     html
